d0336de5b0fc06d35df85d84959775aa0a4f2a1d
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-02-12 16:11:48 +0000

    sass loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './App.scss'
 
 const App = () => {
-  return <div className="App">tesdsdst</div>
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  )
 }
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
   module: {
@@ -15,6 +18,39 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -23,5 +59,9 @@ module.exports = {
       filename: './index.html',
     }),
     new SimpleProgressWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+    }),
   ],
 }

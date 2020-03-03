565c2388590e4c8f51b60449c7253644b9335489
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-03-03 10:47:07 +0000

    react boilerplate

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('true-layer'))
+ReactDOM.render(<App />, document.getElementById('react-app'))
